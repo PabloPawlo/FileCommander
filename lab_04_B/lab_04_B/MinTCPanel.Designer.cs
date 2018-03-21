4d57af2ba5a0f428fb5b6c7b4c848c62a182b00d
PabloPawlo <36933648+PabloPawlo@users.noreply.github.com>
2018-03-22 00:38:58 +0100

    Add files via upload

---
+++ lab_04_B/lab_04_B/MinTCPanel.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace lab_04_B
+{
+    partial class MinTCPanel
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(71, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(122, 30);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(49, 21);
+            this.comboBox1.TabIndex = 1;
+            this.comboBox1.DropDown += new System.EventHandler(this.comboBox1_DropDown);
+            this.comboBox1.SelectedValueChanged += new System.EventHandler(this.DriversCh);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 66);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(171, 251);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.DoubleClick += new System.EventHandler(this.Select);
+            // 
+            // MinTCPanel
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "MinTCPanel";
+            this.Size = new System.Drawing.Size(177, 322);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
---
+++ lab_04_B/lab_04_B/MinTCPanel.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace lab_04_B
+{
+    public partial class MinTCPanel : UserControl
+    {
+        public event Action<MinTCPanel> LoadDrivers;
+        public MinTCPanel()
+        {
+            InitializeComponent();
+        }
+        readonly DriveInfo[] complete = DriveInfo.GetDrives().Where(drive => drive.IsReady).ToArray();
+
+        public string CurrentPath
+        {
+            get
+            {
+                return textBox1.Text;
+            }
+            set
+            {
+                textBox1.Text = value;
+            }
+        }
+        
+        public string[] Dir
+        {
+           
+                set
+                 {
+                    listBox1.Items.Clear();
+                    foreach (string item in value)
+                    {
+                        listBox1.Items.Add(item);
+                    }
+                }
+           
+
+        
+        }
+        public string[] File
+        {
+            set
+            {
+               
+                foreach (string item in value)
+                {
+                    listBox1.Items.Add("   "+item);
+                }
+            }
+        }
+
+
+        private void SetDr()
+        {
+            try
+            {
+                Dir = Directory.GetDirectories(CurrentPath);
+                File = Directory.GetFiles(CurrentPath);
+            }
+            catch
+            {
+            
+            }
+            }
+
+        private void ChangePath(string path)
+        {
+            CurrentPath = path;
+            SetDr();
+        }
+
+      private void DriversCh(object sender, EventArgs e)
+        {
+            var drive = Path.GetPathRoot(comboBox1.SelectedItem.ToString());
+            if (Directory.Exists(drive))
+            {
+                CurrentPath = comboBox1.SelectedItem.ToString();
+                ChangePath(CurrentPath);
+            }
+            else
+            {
+                textBox1.Text = "Napęd nie jest gotowy!";
+                listBox1.Items.Clear();
+            }
+
+        }
+
+
+
+
+      private void comboBox1_DropDown(object sender, EventArgs e)
+        {
+             comboBox1.DataSource = complete;
+        }
+
+        private void Select(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItem != null)
+            {
+                ChangePath(listBox1.SelectedItem.ToString());
+                
+            }
+        }
+
+
+
+
+    }
+   
+
+       
+
+    
+
+
+
+}
+
